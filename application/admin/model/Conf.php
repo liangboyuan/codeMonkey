bb844fdaffff4b08ff656c9f3716f90b723df0d3
liangboyuan <934636851@qq.com>
2019-05-09 10:51:09 +0800

    first commit

---
+++ application/admin/model/Conf.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\admin\model;
+use think\Model;
+class Conf extends Model
+{
+    
+
+    
+
+
+
+
+}
---
+++ application/admin/model/Link.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\admin\model;
+use think\Model;
+class Link extends Model
+{
+    
+
+    
+
+
+
+
+
+}
